7980a74ee5a3c11c676aac7fb9d7fd0581e0ea25
Rupen Kumar Mondal <kumarmondalrupen@gmail.com>
2024-11-11 20:59:54 +0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const alertButton = document.getElementById('alertButton');
-    alertButton.addEventListener('click', function() {
-        alert('Thank you for visiting our site!');
-    });
-});
